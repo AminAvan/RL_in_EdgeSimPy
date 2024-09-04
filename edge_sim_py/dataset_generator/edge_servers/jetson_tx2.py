a1be991445249e01a41af21428dacae23cf0a5c8
AminAvan <amin.avan@ontariotechu.net>
2024-09-04 12:25:40 -0400

    updating the information of cpu_cycle of 'jetson_tx2.py' based on paper named "Multicore implementation of a multichannel parallel graphic equalizer".

--- edge_sim_py/dataset_generator/edge_servers/jetson_tx2.py
+++ edge_sim_py/dataset_generator/edge_servers/jetson_tx2.py
@@ -3,6 +3,11 @@
 [1] Süzen, Ahmet Ali, Burhan Duman, and Betül Şen. "Benchmark Analysis of Jetson TX2, Jetson nano and
     raspberry Pi using Deep-CNN." In 2020 International Congress on Human-Computer Interaction,
     Optimization and Robotic Applications (HORA), pp. 1-5. IEEE, 2020.
+
+[2] Belloch, Jose A., et al. "Multicore implementation of a multichannel parallel graphic equalizer."
+    The Journal of Supercomputing 78.14 (2022): 15715-15729.
+
+        [2] is used for determining the cpu frequency of NVIDIA Jetson TX2
 """
 # EdgeSimPy components
 from edge_sim_py.components.edge_server import EdgeServer
@@ -21,7 +26,7 @@ def jetson_tx2() -> object:
     edge_server.cpu = 4
     edge_server.memory = 8192
     edge_server.disk = 32768
-    edge_server.cpu_cycle = 1734 * (10 ** 6)  ## GHz = 10^9 / MHz = 10^6
+    edge_server.cpu_cycle = 1479 * (10 ** 6)  ## GHz = 10^9 / MHz = 10^6 ##
 
     # Power-related attributes
     edge_server.power_model_parameters = {
