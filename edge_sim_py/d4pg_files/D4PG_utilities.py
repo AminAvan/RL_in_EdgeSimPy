4bdbe6172bf8ad90d8ecf0aff83d7678f5488f28
AminAvan <amin.avan@ontariotechu.net>
2024-09-11 18:51:03 -0400

    Adapting and adjusting the D4PG algorithm for having "generate_complex_normal_distribution".

--- edge_sim_py/d4pg_files/D4PG_utilities.py
+++ edge_sim_py/d4pg_files/D4PG_utilities.py
@@ -8,6 +8,11 @@ import time
 def compute_channel_gain(
     rayleigh_distributed_small_scale_fading: np.ndarray,
     distance: float,
-    path_loss_exponent: int,
+    path_loss_exponent: int,  ## this is determined in
 ) -> np.ndarray:
     return rayleigh_distributed_small_scale_fading / np.power(distance, path_loss_exponent / 2)
+
+
+### Generate a random complex normal distribution, which follows the normal distribution with mean 0 and variance 1
+def generate_complex_normal_distribution(size: int = 1):
+    return np.random.normal(loc=0, scale=1, size=size) + 1j * np.random.normal(loc=0, scale=1, size=size)
