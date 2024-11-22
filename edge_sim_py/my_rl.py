0609f2c6d199e73d49bdfaea01c69cca06837efd
AminAvan <amin.avan@ontariotechu.net>
2024-11-22 12:36:24 -0500

    making ready my_rl.py to be ready for being call by main.py of edgesimpy

--- edge_sim_py/my_rl.py
+++ edge_sim_py/my_rl.py
@@ -1,3 +1,7 @@
+## adopted from 'https://github.com/pytorch/tutorials/blob/main/intermediate_source/reinforcement_q_learning.py'
+# with some modification
+
+
 import gymnasium as gym
 import math
 import random
@@ -68,18 +72,12 @@ class DQN(nn.Module):
         x = F.relu(self.layer2(x))
         return self.layer3(x)
 
-
 steps_done = 0  # Global variable
 
-# Main Function
-def main():
-    # BATCH_SIZE is the number of transitions sampled from the replay buffer
-    # GAMMA is the discount factor as mentioned in the previous section
-    # EPS_START is the starting value of epsilon
-    # EPS_END is the final value of epsilon
-    # EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
-    # TAU is the update rate of the target network
-    # LR is the learning rate of the ``AdamW`` optimizer
+
+
+def initialize():
+    global BATCH_SIZE, GAMMA, EPS_START, EPS_END, EPS_DECAY, TAU, LR
     BATCH_SIZE = 128
     GAMMA = 0.99
     EPS_START = 0.9
@@ -88,66 +86,67 @@ def main():
     TAU = 0.005
     LR = 1e-4
 
-    def initialize():
-        # Get number of actions from gym action space
-        n_actions = env.action_space.n
-        # Get the number of state observations
-        state, info = env.reset()
-        n_observations = len(state)
+    global policy_net, target_net, optimizer, memory  # Declare these as global variables
+    # Get number of actions from gym action space
+    n_actions = env.action_space.n
+    # Get the number of state observations
+    state, info = env.reset()
+    n_observations = len(state)
 
-        policy_net = DQN(n_observations, n_actions).to(device)
-        target_net = DQN(n_observations, n_actions).to(device)
-        target_net.load_state_dict(policy_net.state_dict())
+    policy_net = DQN(n_observations, n_actions).to(device)
+    target_net = DQN(n_observations, n_actions).to(device)
+    target_net.load_state_dict(policy_net.state_dict())
 
-        optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
-        memory = ReplayMemory(10000)
+    optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
+    memory = ReplayMemory(10000)
 
 
-    def select_action(state):
-        global steps_done
-        sample = random.random()
-        eps_threshold = EPS_END + (EPS_START - EPS_END) * \
-                        math.exp(-1. * steps_done / EPS_DECAY)
-        steps_done += 1
-        if sample > eps_threshold:
-            with torch.no_grad():
-                # t.max(1) will return the largest column value of each row.
-                # second column on max result is index of where max element was
-                # found, so we pick action with the larger expected reward.
-                return policy_net(state).max(1).indices.view(1, 1)
+def select_action(state):
+    global steps_done
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+                    math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+    if sample > eps_threshold:
+        with torch.no_grad():
+            # t.max(1) will return the largest column value of each row.
+            # second column on max result is index of where max element was
+            # found, so we pick action with the larger expected reward.
+            return policy_net(state).max(1).indices.view(1, 1)
+    else:
+        return torch.tensor([[env.action_space.sample()]], device=device, dtype=torch.long)
+
+episode_durations = []
+
+def plot_durations(show_result=False):
+    plt.figure(1)
+    durations_t = torch.tensor(episode_durations, dtype=torch.float)
+    if show_result:
+        plt.title('Result')
+    else:
+        plt.clf()
+        plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(durations_t.numpy())
+    # Take 100 episode averages and plot them too
+    if len(durations_t) >= 100:
+        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
+
+    plt.pause(0.001)  # pause a bit so that plots are updated
+    if is_ipython:
+        if not show_result:
+            display.display(plt.gcf())
+            display.clear_output(wait=True)
         else:
-            return torch.tensor([[env.action_space.sample()]], device=device, dtype=torch.long)
+            display.display(plt.gcf())
 
-    episode_durations = []
-
-    def plot_durations(show_result=False):
-        plt.figure(1)
-        durations_t = torch.tensor(episode_durations, dtype=torch.float)
-        if show_result:
-            plt.title('Result')
-        else:
-            plt.clf()
-            plt.title('Training...')
-        plt.xlabel('Episode')
-        plt.ylabel('Duration')
-        plt.plot(durations_t.numpy())
-        # Take 100 episode averages and plot them too
-        if len(durations_t) >= 100:
-            means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
-            means = torch.cat((torch.zeros(99), means))
-            plt.plot(means.numpy())
-
-        plt.pause(0.001)  # pause a bit so that plots are updated
-        if is_ipython:
-            if not show_result:
-                display.display(plt.gcf())
-                display.clear_output(wait=True)
-            else:
-                display.display(plt.gcf())
-
-    ###############
-    # Training loop
-    # ^^^^^^^^^^^^^
+###############
+# Training loop
+# ^^^^^^^^^^^^^
+def rl_training():
     def optimize_model():
         if len(memory) < BATCH_SIZE:
             return
@@ -241,9 +240,3 @@ def main():
     plot_durations(show_result=True)
     plt.ioff()
     plt.show()
-
-
-# Conditional Execution
-if __name__ == "__main__":
-    main()
-
