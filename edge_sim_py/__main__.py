c149213050f96d875522cc53b1b7d0f86dd93108
AminAvan <amin.avan@ontariotechu.net>
2024-12-09 01:28:09 -0500

    updating 'compute_reward'.

--- edge_sim_py/__main__.py
+++ edge_sim_py/__main__.py
@@ -1089,6 +1089,8 @@ def my_rl_in_edgesimpy(parameters):
                 truncated = True
             elif num_likely_missed_deadline >= (11 * (len(Service.all()) * len(EdgeServer.all()))) and (count_ones < (0.99 * (len(Service.all())))):
                 truncated = True
+            elif num_likely_missed_deadline >= (12 * (len(Service.all()) * len(EdgeServer.all()))):
+                truncated = False
             else:
                 truncated = False
 
