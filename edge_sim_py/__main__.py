95ea49abdc0e62bd2fbf327e8340a6fe79097907
AminAvan <aminavan.hardware@yahoo.com>
2024-12-21 20:11:11 -0500

    logging the data in a csv file for 31 run of normal_RL

--- edge_sim_py/__main__.py
+++ edge_sim_py/__main__.py
@@ -1654,7 +1654,7 @@ def wrapped_Service_Provisioning(parameters, algorithm_name=scheduling_algorithm
 
     # for testing RL for 31 runs
     # Get the function based on the algorithm name
-    for i in range(2):
+    for i in range(31):
         result = algorithm_functions[algorithm_name](parameters)
     process = psutil.Process(os.getpid())
     resource_tracker.update(process.memory_info().rss)
